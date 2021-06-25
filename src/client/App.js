919b5e0505cf152ef505cd217124d000fcb25832
mikahimself <mika.laihanen@gmail.com>
2021-06-25 20:15:08 +0300

    Clear date when area changes

--- src/client/App.js
+++ src/client/App.js
@@ -8,25 +8,19 @@ const ENDPOINT = "http://localhost:3030";
 function App() {
   const [reply, setReply] = useState("");
   const [areas, setAreas] = useState([]);
-  const [selectedArea, setSelectedArea] = useState(null)
+  const [selectedArea, setSelectedArea] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
   const [dates, setDates] = useState([]);
   const socket = io(ENDPOINT);
   socket.on("connection", () => {
     console.log("Connected to server.")
   });
 
-  // useEffect(() => {
-  //   async function getShowsForArea(area) {
-  //     fetchAreaDate
-  //   }
-  // }, [selectedArea])
-
   useEffect(() => {
     let lsAreas = JSON.parse(window.localStorage.getItem("areas"));
     async function fetchAreaDates(areas) {
       for (let area of areas) {
         let dates = await fetchAreaDate(area.id);
-        // console.log(dates)
         area.dates = dates;
       }
     }
@@ -39,7 +33,6 @@ function App() {
       .then(data => JSON.parse(xmljs.xml2json(data, {compact: true, spaces: 4})))
       .then(data => data.TheatreAreas.TheatreArea.map(entry => { return { "id": entry.ID._text, "name": entry.Name._text }}))
       setAreas(await areaData);
-      // fetchAreaDates(await areaData);
       window.localStorage.setItem("areas", JSON.stringify(await areaData))
     };
     fetchTheatreAreas();}
@@ -47,24 +40,13 @@ function App() {
       console.log("use local storage")
       let lsAreas = JSON.parse(window.localStorage.getItem("areas"));
       fetchAreaDates(lsAreas);
-      // for (let area of lsAreas) {
-      //   let dates = fetchAreaDate(area.id);
-      //   console.log(dates)
-      // }
-      //console.log(lsAreas)
       setAreas(lsAreas);
-      //window.localStorage.setItem("areas", JSON.stringify("poop"))
-      //window.localStorage.setItem("areas", JSON.stringify(lsAreas))
-      //console.log(dates)
-      //window.localStorage.setItem("areas", JSON.stringify(dates))
-      //fetchAreaDate({id: 1038})
     }
   }, []);
 
 
 
   const fetchAreaDate = async(area) => {
-    
     let url = `https://www.finnkino.fi/xml/ScheduleDates/?area=${area}`;
     let res = await fetch(url);
     let resText = await res.text();
@@ -78,11 +60,12 @@ function App() {
     const selectedIndex = e.target.options.selectedIndex;
     const selection = e.target.options[selectedIndex].getAttribute('data-id');
     setSelectedArea(selection);
+    setSelectedDate("");
     fetchAreaDate(selection);
   }
 
-  const updateDates = () => {
-    console.log("Date change")
+  const updateDates = (e) => {
+    setSelectedDate(e.target.value);
   }
 
   return (
@@ -90,20 +73,17 @@ function App() {
       <h1>It sorta works.</h1>
       {reply && <p>Reply from server: {reply}</p>}
       {areas && (
-        <select onChange={updateSelectedArea} >
+        <select onChange={updateSelectedArea} value={selectedArea}>
           <option value="null">Select theatre</option>
-          {areas.map(area => <option key={area.id} data-id={area.id}>{area.name}: {area.id}</option>) }
+          {areas.map(area => <option key={area.id} value={area.id} data-id={area.id}>{area.name}: {area.id}</option>) }
         </select>
       )}
       {dates && (
-        <select onChange={updateDates}>
+        <select onChange={updateDates} value={selectedDate}>
           <option value="null">Select date</option>
-          {dates.map(date => <option key={date}>{date}</option>)}
+          {dates.map(date => <option key={date} value={date}>{date}</option>)}
         </select>  
-      )
-
-      }
-
+      )}
     </div>
   );
 }
